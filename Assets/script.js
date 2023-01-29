b49a18862784c737faab91ce1e7857af22f34089
Robbinpass <robbinsandrew@outlook.com>
2023-01-29 11:53:11 -0500

    Added current date text to page

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,24 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentdayel = $('#currentday');
+
+var today = dayjs();
+$('#currentDay').text(today.format('[Today is] dddd'));
+$('#dayOfWeek').text(today.format('MMM D, YYYY'));
+
+
+
+
+
+
+console.log(currentdayel);
+
+
+
 $(function () {
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,3 +38,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
